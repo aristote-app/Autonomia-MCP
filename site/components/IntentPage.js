85e6d8c49f2417119abac733cd01c9dddea7ab45
Dian Goldcher <deborahdiangoldcher@gmail.com>
2026-09-23 21:24:56 +0200

    fix(site): normalize component imports

--- site/components/IntentPage.js
+++ site/components/IntentPage.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import LeadForm from "@/components/LeadForm";
 import QualiopiProof from "@/components/QualiopiProof";
-import AutonomiaScan from "@/components/AutonomiaScan";\nimport LiveExpertProfiles from "@/components/LiveExpertProfiles";
+import AutonomiaScan from "@/components/AutonomiaScan";
+import LiveExpertProfiles from "@/components/LiveExpertProfiles";
 
 export default function IntentPage({ page }) {
   const isExpert = page.mode === "experts";
